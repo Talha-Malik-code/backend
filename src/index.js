062141b771579c8536f6d735eff8ea33328e6e88
Talha Malik <t4talhawork@gmail.com>
2025-07-12 05:50:47 +0500

    utils code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 // require("dotenv").config({path:"./env"});
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: "./env",
 });
 
-connectDB();
+connectDB()
+.then(() => {
+    const port = process.env.PORT || 8000;
+    app.listen(port, () => {
+        console.log(`Listening at Port: ${port}`);
+    })
+})
+.catch((error) => {
+    console.log(" Error connecting db: ", error);
+});
