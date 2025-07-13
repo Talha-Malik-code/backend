51512e00aa3dba5691ca579c1c24d268c67977e6
Talha Malik <t4talhawork@gmail.com>
2025-07-13 11:34:55 +0500

    fix all register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -32,14 +32,19 @@ const registerUser = asyncHandler( async (req, res) => {
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path;
+    }
 
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar is required");
     }
 
-    const avatar = uploadOnCloudinary(avatarLocalPath);
-    const coverImage = uploadOnCloudinary(coverImageLocalPath);
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
 
     if (!avatar) {
         throw new ApiError(400, "Avatar is required");
@@ -63,7 +68,7 @@ const registerUser = asyncHandler( async (req, res) => {
     }
 
     return res.status(201).json(
-        ApiResponse(200, createdUser, "User created Successfully!")
+        new ApiResponse(200, createdUser, "User created Successfully!")
     )
 
 } );
