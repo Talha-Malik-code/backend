336c08273ab77db9ad8c39b62de4995369ccbd9f
Talha Malik <t4talhawork@gmail.com>
2025-07-17 17:38:38 +0500

    Add healtcheck endpoint

--- src/app.js
+++ src/app.js
@@ -16,9 +16,11 @@ app.use(cookieParser());
 
 // routes import
 import userRouter from "./routes/user.routes.js";
+import healthcheckRouter from "./routes/healthcheck.routes.js";
 
 
 //routes decleration
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/healthcheck", healthcheckRouter);
 
 export { app }
